513d6d601e1a5c609633afc9ea6478b5305c91fb
programmer-alt <modstils88@mail.ru>
2023-07-15 21:35:25 +0400

    при наведении на кнопку срабатывает два разных класса рандомным методом

--- animation.js
+++ animation.js
@@ -1,13 +1,23 @@
-var button = document.querySelector('.animation');
-var flowText = document.querySelector('.anim');
+var button = document.querySelector('.animation-button');
+var flowText = document.querySelector('.animText');
 var go = document.querySelector('.go');
+function newClassCss() {
+    var randomClass = Math.floor(Math.random() * 10);
+    var newClass = (randomClass >= 5) ? 'anim-mirrow' : 'anim-mirrowTwo';
+    flowText.classList.add(newClass);
+    if (newClass === 'anim-mirrow') {
+        flowText.style.animationName = 'rainbow';
+    }
+    else if (newClass === 'anim-mirrowTwo') {
+        flowText.style.animationName = 'rainbow-mirrow';
+    }
+}
 function animation() {
-    flowText.classList.add('anim');
+    newClassCss();
     flowText.style.visibility = 'visible';
     go.style.visibility = 'visible';
 }
 function animationRemove() {
-    flowText.classList.remove('anim');
     flowText.style.visibility = 'hidden';
     go.style.visibility = 'hidden';
 }
